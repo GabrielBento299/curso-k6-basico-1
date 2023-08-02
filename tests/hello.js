7ef3dc20ead7333b7cbf82fdf9cccb8ea15632d5
Gabriel Bento <gabrielbento299@hotmail.com>
2023-08-02 11:59:01 -0300

    report added k6

--- tests/hello.js
+++ tests/hello.js
@@ -1,6 +1,14 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+};
+
 export default function () {
   const response = http.get('http://localhost:3333');
   check(response, {
@@ -8,4 +16,4 @@ export default function () {
   });  
   
   sleep(1);
-}
+};
